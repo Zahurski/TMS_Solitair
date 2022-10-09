67eb68f39ed2ab24315f0b8971b326be5893db44
Zakhar <z.zahurski@gmail.com>
2022-10-10 00:15:16 +0300

    added project

---
+++ Assets/Scripts/CardPlace.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using UnityEngine;
+
+public class CardPlace : MonoBehaviour
+{
+    [SerializeField] protected int nextCardValue = 11;
+    [SerializeField] protected float onGameZOffset = -2f;
+    [SerializeField] protected CardColor nextCardColor = CardColor.Any;
+    [SerializeField] protected CardType nextCardType = CardType.Any;
+    [SerializeField] protected Transform cardContainer;
+    [SerializeField] protected bool isMain;
+
+    protected bool _isOpen = true;
+
+    public int NextCardValue => nextCardValue;
+
+    public CardColor NextCardColor => nextCardColor;
+
+    public CardType NextCardType => nextCardType;
+
+    public Transform CardContainer => cardContainer;
+
+    public bool IsMain => isMain;
+
+    public bool IsOpen => _isOpen;
+
+    public bool IsCanConnect(PlayingCard playingCard)
+    {
+        if (!_isOpen) 
+            return false;
+        if (playingCard.Value != nextCardValue && nextCardValue != -1) 
+            return false;
+        if (nextCardColor != CardColor.Any && playingCard.Color != nextCardColor) 
+            return false;
+        if (nextCardType != CardType.Any && playingCard.Type != nextCardType) 
+            return false;
+
+        Vector3 position = playingCard.CardContainer.transform.localPosition;
+        position.z = isMain ? 0f : onGameZOffset;
+        playingCard.CardContainer.localPosition = position;
+        return true;
+    }
+}
